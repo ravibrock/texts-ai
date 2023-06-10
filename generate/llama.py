0018d11491596a78bce6b78801d61151b2bfeba0
ravibrock <66334356+ravibrock@users.noreply.github.com>
2023-06-10 09:48:12 -0400

    Update llama.py

--- generate/llama.py
+++ generate/llama.py
@@ -1,9 +1,11 @@
 from llama_cpp import Llama
-import os.path as osp
+import os
 
 
 def gen(prompt, model="models/llama", max_tokens=64):
-    llm = Llama(model_path=osp.expanduser(model))
+    llm = Llama(model_path=os.path.expanduser(model))
     output = llm(prompt, max_tokens=max_tokens, echo=False)
+    reply = output["choices"][0]["text"]
+    reply_firstline = reply.split("\n")[0]
 
-    return output["choices"][0]["text"]
+    return reply_firstline
