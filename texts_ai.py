506a415016af3499b20362fe0aac484c81fde40c
ravibrock <66334356+ravibrock@users.noreply.github.com>
2023-06-11 13:25:50 -0400

    Concatenate multiple messages sent sequentially

--- texts_ai.py
+++ texts_ai.py
@@ -11,7 +11,7 @@ import datetime
 class suppress_stdout_stderr(object):
     def __init__(self):
         # Open a pair of null files
-        self.null_fds = [os.open(os.devnull, os.O_RDWR) for x in range(2)]
+        self.null_fds = [os.open(os.devnull, os.O_RDWR) for _ in range(2)]
         # Save the actual stdout (1) and stderr (2) file descriptors.
         self.save_fds = [os.dup(1), os.dup(2)]
 
@@ -36,12 +36,24 @@ def transform_date(date):
     return new_date
 
 
-def get_message(db_file, sql):
+def get_message(db_file, sql, delay):
+    msg = []
     start_time = int(datetime.datetime.now().timestamp())
     while True:
         messages = custom_sql.query(db_file, sql)
         if transform_date(messages[0][2]) > start_time:
-            return messages[0][0], messages[0][3]
+            contact = messages[0][0]
+            time.sleep(delay)
+            messages = custom_sql.query(db_file, sql)
+            if transform_date(messages[0][2]) > start_time:
+                for message in messages:
+                    if transform_date(message[2]) > start_time:
+                        if message[0] == contact:
+                            msg.append(message[3])
+                    else:
+                        break
+            msg.reverse()
+            return contact, " ".join(msg)
         else:
             time.sleep(1)
 
@@ -59,11 +71,10 @@ def send_message(contact, message):
     print(f"Sent to {contact}: {message}")
 
 
-def monitor(gen_function, model_path, db_file, sql):
+def monitor(gen_function, model_path, db_file, sql, delay):
     print("Monitoring...")
     while True:
-        contact, message = get_message(db_file, sql)
-        # TODO: Add optional delay to wait for multiple messages
+        contact, message = get_message(db_file, sql, delay)
         multiprocessing.Process(target=dispatch_reply, args=(gen_function, model_path, contact, message)).start()
 
 
@@ -71,6 +82,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("model_type", choices=["gpt", "llama"], help="type of model to use")
     parser.add_argument("model_path", type=str, help="path/to/model")
+    parser.add_argument("--delay", type=int, default=5, help="delay between checks for new messages")
     args = parser.parse_args()
 
     if args.model_type == "gpt":
@@ -84,7 +96,7 @@ def main():
     with open("sql/new_messages.sql", "r") as sql_file:
         sql = sql_file.read().replace("\\n", "\n")
 
-    monitor(gen_function, args.model_path, db_file, sql)
+    monitor(gen_function, args.model_path, db_file, sql, args.delay)
 
 
 if __name__ == "__main__":
